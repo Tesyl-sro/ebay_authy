f50b641ed37150887ec50d460a5ba19e3b59e6ab
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-08-15 16:54:48 +0200

    add credential setup

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using eBay.ApiClient.Auth.OAuth2;
 using eBay.ApiClient.Auth.OAuth2.Model;
+using ebay_authy.Helpers;
 using Spectre.Console;
 using Utils = ebay_authy.Utilities.Utils;
 
@@ -17,12 +18,30 @@ class Program
         Utils.Greet();
         var keyset = Utils.AskKeyset();
 
+        TryCredentialSetup(keyset);
+
         var api = new OAuth2Api();
         var appToken = GetApplicationToken(api);
         var authUrl = api.GenerateUserAuthorizationUrl(ENVIRONMENT, SCOPES, "");
 
         AnsiConsole.MarkupLine("[green]Please log in with the following URL:[/] " + authUrl);
         Utils.AskOpenUrl(authUrl);
+    }
+
+    private static void TryCredentialSetup(Keyset keyset)
+    {
+        var keysetStream = keyset.ConvertToStream();
+        var reader = new StreamReader(keysetStream);
+
+        try
+        {
+            CredentialUtil.Load(reader);
+        }
+        catch (Exception e)
+        {
+            AnsiConsole.MarkupLine("[red bold]Error while setting up credentials:[/] " + e.Message);
+            Environment.Exit(1);
+        }
     }
 
     private static string GetApplicationToken(OAuth2Api api)
